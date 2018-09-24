dc68bea30a8c76436d813c77648be8153773d133
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2018-09-24 19:21:12 -0300

    refact(*): Complete role refactor, removing variables that could be empty (unsetted). Using continuous integration and automated test to ensure that everything is working properly

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-APPDIR=/var/tests
+ROLE_DIR=/var/tests
 CURRENT_DIR=$(dirname $0)
 [[ "${CURRENT_DIR}" == '.' ]] && CURRENT_DIR=$(pwd)
 IMAGE=williamyeh/ansible:ubuntu16.04
+CONTAINER_NAME="${CURRENT_DIR##*/}-$(date +"%Y-%m-%d")"
 
-RUNNER=$(docker run -v $CURRENT_DIR:$APPDIR \
-    -w $APPDIR -t -i \
-    -d $IMAGE bash)
+_CONTAINER=$(docker ps -a -f "name=${CONTAINER_NAME}" \
+        -f "ancestor=${IMAGE}" \
+        --format '{{ .Names }}' | head -n 1)
+
+[ -z "${_CONTAINER}" ] && {
+    RUNNER=$(docker run -it --name $CONTAINER_NAME \
+        -v $CURRENT_DIR:$ROLE_DIR \
+        -w $ROLE_DIR \
+        -d $IMAGE bash)
+} || {
+    RUNNER=${_CONTAINER}
+}
 
 assert () {
     docker exec -it $RUNNER $1 || \
@@ -15,11 +25,14 @@ assert () {
 }
 
 {
-    assert "ansible-playbook -c local --syntax-check test.yml"          &&
-    assert "ansible-playbook -c local test.yml"
+    assert "ansible-galaxy install -r requirements.yml" &&
+    assert "ansible-playbook -c local --syntax-check test.yml" &&
+    assert "ansible-playbook -c local test.yml" &&
+    assert "ansible-playbook -c local test.yml" \
+        | grep -q 'changed=0.*failed=0'
 } || {
     echo "Tests are failed"
     docker exec -it $RUNNER /bin/bash
 }
 
-docker rm $(docker stop $RUNNER)
+# docker rm $(docker stop $RUNNER)
