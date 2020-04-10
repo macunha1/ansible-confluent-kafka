ef23b4c9585794b8d7cf694a08e36260597bf744
Matheus Cunha <12192633+macunha1@users.noreply.github.com>
2020-04-10 10:21:24 +0200

    feat(ci): Including Alpine Linux container to the CI
    Adding Alpine Linux support on the list of testing OS. Alpine Linux
    build will execute inside a Docker container carrying Python 2 with
    OpenJDK at the moment.
    
    Later, it's planned to also include OpenJDK 11 and Python 3 for Alpine
    Linux.

--- runtests.sh
+++ runtests.sh
@@ -1,40 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export WORKDIR=$(cd $(dirname "$0") && pwd)
 
-# DOCKER_TEST_IMAGE=macunha1/ansible:ubuntu-20.04
-DOCKER_TEST_IMAGE=macunha1/alpine-3.11.5-java
+DOCKER_TEST_IMAGE=macunha1/ansible:alpine-3.11.5
 CONTAINER_NAME="${WORKDIR##*/}-$(date +"%Y-%m-%d")"
 
 DOCKER_CONTAINER=$(docker ps -a -f "name=${CONTAINER_NAME}" \
-        -f "ancestor=${DOCKER_TEST_IMAGE}" \
-        --format '{{ .Names }}' | head -n 1)
+	-f "ancestor=${DOCKER_TEST_IMAGE}" \
+	--format '{{ .Names }}' | head -n 1)
 
 [ -z "${DOCKER_CONTAINER}" ] && {
-    ROLE_DIR=/usr/local/opt
+	ROLE_DIR=/usr/local/opt
 
-    RUNNER=$(docker run -it --name ${CONTAINER_NAME} \
-        --rm \
-        -v ${WORKDIR}:${ROLE_DIR} \
-        -w ${ROLE_DIR} \
-        --entrypoint "sh" \
-        -d $DOCKER_TEST_IMAGE)
+	RUNNER=$(docker run -it --name ${CONTAINER_NAME} \
+		--rm \
+		-v ${WORKDIR}:${ROLE_DIR} \
+		-w ${ROLE_DIR} \
+		--entrypoint "sh" \
+		-d $DOCKER_TEST_IMAGE)
 } || {
-    RUNNER=${DOCKER_CONTAINER}
+	RUNNER=${DOCKER_CONTAINER}
 }
 
-assert () {
-    docker exec -it $RUNNER $1 || \
-        ( echo 'Test for command "'${1}'" just failed' && exit 1 )
+assert() {
+	docker exec -it $RUNNER $1 ||
+		(echo 'Test for command "'${1}'" just failed' && exit 1)
 }
 
 {
-    assert "pip install ansible==2.9.6"
-    assert "ansible-playbook -c local --syntax-check test.yml" &&
-    assert "ansible-playbook -c local test.yml"
+	assert "pip install ansible==2.9.6"
+	assert "ansible-playbook -c local --syntax-check test.yml" &&
+		assert "ansible-playbook -c local test.yml"
 
-    docker stop $RUNNER
+	# docker stop $RUNNER
 } || {
-    echo "Tests are failing"
-    docker exec -it $RUNNER /bin/sh
+	echo "Tests are failing"
+	docker exec -it $RUNNER /bin/sh
 }
